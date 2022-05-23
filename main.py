a90346cb748238e2463dd92ed1431b5cca88189e
RayJi <kaikai0428@gmail.com>
2022-05-24 01:36:14 +0800

    update

--- main.py
+++ main.py
@@ -15,39 +15,57 @@ url = os.environ['SK_URL']
 
 # 定義發送訊息
 today = date.today()
-msg = "{p_url}\n跨部門新增{p_date}".format(
+msg = "(sun) [跨部門新增{p_date}]({p_url}) (sun)".format(
     p_date=today.strftime("%m%d"), p_url=url)
 # 要先安裝ChromeDriver才能使用
 # https://chromedriver.chromium.org/downloads
 browser = webdriver.Chrome('chromedriver_101.exe')
 browser.set_window_size(200, 500)
 browser.get('https://web.skype.com/')
+
+# 確認帳號
 loginElem = browser.find_element_by_name('loginfmt')
 loginElem.send_keys(accout)
-nextElem = browser.find_element_by_id('idSIButton9')  # 確認帳號
+nextElem = browser.find_element_by_id('idSIButton9')
 nextElem.click()
 sleep(1)
+
+# 確認密碼
 pwdElem = browser.find_element_by_name('passwd')
 pwdElem.send_keys(password)
-nextElem = browser.find_element_by_id('idSIButton9')  # 確認密碼
+nextElem = browser.find_element_by_id('idSIButton9')
 nextElem.click()
 sleep(1)
-nextElem = browser.find_element_by_id('idSIButton9')  # 確認記憶帳號
+
+# 確認記憶帳號
+nextElem = browser.find_element_by_id('idSIButton9')
 nextElem.click()
-# pwdElem.submit()
-sleep(5)  # 需要讀取時間
-groupElem = browser.find_element_by_xpath(
-    "//div[@data-text-as-pseudo-element='我']")  # //標籤@屬性='值'
+
+# 尋找對話視窗
+# Skype需要時間load有可能一開始元素不存在會跳錯
+# 因為頁面可視範圍有限,把目標對話視窗放到"我的最愛"比較容易找到
+groupElem = None
+while groupElem == None:
+    try:
+        groupElem = browser.find_element_by_xpath(
+            "//div[@data-text-as-pseudo-element='幹話討論']")  # //標籤@屬性='值'
+    except:
+        print("找不到元素...")
+        sleep(1)
 # https://blog.csdn.net/WanYu_Lss/article/details/84137519
+# 有些想要點擊的元素藏在第二層, 改用網路找到的其他方法處理
 browser.execute_script("arguments[0].click();", groupElem)
 # groupElem.click()
 sleep(1)
+
+# 輸入訊息
 inputElem = browser.find_element_by_xpath(
     "//div[@role='textbox']")  # //標籤@屬性='值'
 inputElem.send_keys(msg)  # 發送訊息
 sendElem = browser.find_element_by_xpath(
     "//button[@aria-label='Send message']")  # //標籤@屬性='值'
 sendElem.click()
-
 sleep(1)
-browser.quit()  # 關閉
+
+# 關閉程式
+browser.quit()
