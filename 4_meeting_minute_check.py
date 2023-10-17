1bc3be9855081b0edbd2cd6b9a71c1a875f42dba
RayJi <kaikai0428@gmail.com>
2023-10-17 15:22:05 +0800

    重新整理 提煉共用邏輯

---
+++ 4_meeting_minute_check.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+from datetime import date, timedelta
+import os
+import sys
+import _ray_selenium
+
+# 先切換到當前目錄，才能正常讀取檔案
+cwd_dir = os.path.dirname(sys.argv[0])
+os.chdir(cwd_dir)
+
+url = 'https://docs.google.com/spreadsheets/d/1zvqC3UeDDrfO-g52PGG-MxbTwK9KwDQ6xDhkI6XkB0o/edit#gid=828109937'
+meet_date = date.today() + timedelta(days=-1)
+msg = f'[{meet_date.strftime("%m/%d")}會議記錄請確認(明哥區塊、本組區塊以及紅字)，預計下班前提交]({url})'
+target = '幹話討論'
+
+browser = _ray_selenium.createBrowser()
+_ray_selenium.login()
+
+# 尋找對話視窗
+# Skype需要時間load有可能一開始元素不存在會跳錯
+# 因為頁面可視範圍有限,把目標對話視窗放到"我的最愛"比較容易找到
+groupElem = _ray_selenium.findElementForever(
+    By.XPATH, "//div[@data-text-as-pseudo-element='{target}']".format(target=target))  # //標籤@屬性='值'
+
+# https://blog.csdn.net/WanYu_Lss/article/details/84137519
+# 有些想要點擊的元素藏在第二層, 改用網路找到的其他方法處理
+browser.execute_script("arguments[0].click();", groupElem)
+# groupElem.click()
+sleep(1)
+
+# 輸入訊息
+inputElem = _ray_selenium.findElementForever(
+    By.XPATH, "//div[@role='textbox']")  # //標籤@屬性='值'
+inputElem.send_keys(msg)  # 發送訊息
+sendElem = _ray_selenium.findElementForever(
+    By.XPATH, "//button[@aria-label='傳送訊息']")  # //標籤@屬性='值'
+sendElem.click()
+sleep(3)
+
+# 關閉程式
+browser.quit()
---
+++ _ray_selenium.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+from datetime import date, timedelta
+import os
+import sys
+
+browser = None
+sk_account = os.environ['SK_ACCOUNT']
+sk_pwd = os.environ['SK_PWD']
+
+
+# 尋找物件
+def findElementForever(myBy, myValue):
+    elem = None
+    while elem == None:
+        try:
+            elem = browser.find_element(
+                by=myBy, value=myValue)  # //標籤@屬性='值'
+            if elem.is_displayed() == False:
+                raise Exception('尚未displayed')
+        except Exception as arg:
+            print(f"找不到元素...{myValue}")
+            sleep(1)
+    return elem
+
+
+# 建立瀏覽器
+def createBrowser():
+    global browser
+    s = Service('chromedriver.exe')
+    browser = webdriver.Chrome(service=s)
+    browser.set_window_size(200, 500)
+    browser.get('https://web.skype.com/')
+    return browser
+
+
+# 登入
+def login():
+    # 確認帳號
+    loginElem = findElementForever(By.NAME, 'loginfmt')
+    loginElem.send_keys(sk_account)
+    nextElem = findElementForever(By.ID, 'idSIButton9')
+    nextElem.click()
+    sleep(1)
+
+    # 確認密碼
+    pwdElem = findElementForever(By.NAME, 'passwd')
+    pwdElem.send_keys(sk_pwd)
+    nextElem = findElementForever(By.ID, 'idSIButton9')
+    nextElem.click()
+    sleep(1)
+
+    # 確認記憶帳號
+    nextElem = findElementForever(By.ID, 'idSIButton9')
+    nextElem.click()
+    sleep(1)
